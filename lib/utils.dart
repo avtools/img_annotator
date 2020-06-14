6ed0d22cd2d33f20900d8aef22bb1448231283d1
pavithranp <ptpn94@gmail.com>
2020-06-15 00:51:20 +0200

    dart reformating

---
+++ lib/utils.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'dart:io';
+
+class AppUtil {
+  static Future<String> createFolderInAppDocDir(String folderName) async {
+    //Get this App Document Directory
+    final Directory _appDocDir = await getApplicationDocumentsDirectory();
+    //App Document Directory + folder name
+    final Directory _appDocDirFolder =
+        Directory('${_appDocDir.path}/$folderName/');
+
+    if (await _appDocDirFolder.exists()) {
+      //if folder already exists return path
+      return _appDocDirFolder.path;
+    } else {
+      //if folder not exists create folder and then return its path
+      final Directory _appDocDirNewFolder =
+          await _appDocDirFolder.create(recursive: true);
+      return _appDocDirNewFolder.path;
+    }
+  }
+}
