6ed1a86ef88d23345a6094b2d6fc786e635fbc01
pavithranp <ptpn94@gmail.com>
2020-06-19 18:35:20 +0200

    fix nav bugs

--- lib/main.dart
+++ lib/main.dart
@@ -2,87 +2,64 @@ import 'dart:async';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-
-import 'capture.dart';
+import 'package:img_annotator/capture.dart';
 
 Future<void> main() async {
   // Ensure that plugin services are initialized so that `availableCameras()`
   // can be called before `runApp()`
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Obtain a list of the available cameras on the device.
+// Obtain a list of the available cameras on the device.
   final cameras = await availableCameras();
-
-  // Get a specific camera from the list of available cameras.
-  final firstCamera = cameras.first;
 
   runApp(
     MaterialApp(
       theme: ThemeData.dark(),
-      title: 'Welcome to Flutter',
+      title: 'Image Annotator',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to Flutter'),
 
         ),
         body: Column(
-          children: [titleSection],
+          children: [ HomePage(camera: cameras.first)],
         ),
       ),
     ),
   );
 }
 
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        /*1*/
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /*2*/
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: Text(
-                'Oeschinen Lake Campground',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+
+class HomePage extends StatelessWidget {
+  HomePage({CameraDescription camera}) : this.this_camera = camera;
+  final CameraDescription this_camera;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.grey[950],
+      child: Center(
+        child: Ink(
+          decoration: const ShapeDecoration(
+            color: Colors.lightBlue,
+            shape: CircleBorder(),
+          ),
+          child: IconButton(
+            icon: Icon(Icons.android),
+            color: Colors.white,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TakePictureScreen(camera: this_camera),
                 ),
-              ),
-            ),
-            Text(
-              'Kandersteg, Switzerland',
-              style: TextStyle(
-                color: Colors.grey[500],
-              ),
-            ),
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.redAccent,
-              child: Text('Back to Main Page'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DisplayPictureScreen(imagePath: path),
-                  ),
-                );
-              },
-            ),
-          ],
+              );
+            },
+          ),
         ),
       ),
-      /*3*/
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      Text('41'),
-    ],
-  ),
-);
+    );
+  }
+}
 
 
 //TakePictureScreen(
